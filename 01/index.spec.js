80fe4bdc9d5e5b923ced9618b6b727ff45eb5c8b
zyhou <richard.maxime9@gmail.com>
2020-12-01 18:51:33 +0100

    add first day solution

--- 01/index.spec.js
+++ 01/index.spec.js
@@ -1,7 +1,53 @@
-const sum = require("./index");
+const { solveOne, solveTwo } = require("./index");
 
-describe("sum", () => {
-  it("should adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
+describe("Day 01", () => {
+  describe("Solve One", () => {
+    it("should return 514579 with input", () => {
+      const input = `1721
+979
+366
+299
+675
+1456`;
+
+      expect(solveOne(input)).toBe(514579);
+    });
+
+    it("should throw when input sum is not 2020", () => {
+      const input = `
+0
+1
+2
+3`;
+
+      expect(() => {
+        solveOne(input);
+      }).toThrow();
+    });
+  });
+
+  describe("Solve Two", () => {
+    it("should return 241861950 with input", () => {
+      const input = `1721
+    979
+    366
+    299
+    675
+    1456`;
+
+      expect(solveTwo(input)).toBe(241861950);
+    });
+
+    it("should throw when input sum is not 2020", () => {
+      const input = `
+0
+1
+2
+3`;
+
+      expect(() => {
+        solveTwo(input);
+      }).toThrow();
+    });
   });
 });
