081e24774355a97e9533bc098fa5cb5f6a9d3612
zyhou <richard.maxime9@gmail.com>
2020-12-02 17:24:17 +0100

    add second day

---
+++ 02/index.spec.js
@@ -0,0 +1,23 @@
+const { solveOne, solveTwo } = require("./index");
+
+describe("Day 02", () => {
+  describe("Solve One", () => {
+    it("should return 2 with input", () => {
+      const input = `1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc`;
+
+      expect(solveOne(input)).toBe(2);
+    });
+  });
+
+  describe("Solve Two", () => {
+    it("should return 1 with input", () => {
+      const input = `1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc`;
+
+      expect(solveTwo(input)).toBe(1);
+    });
+  });
+});
